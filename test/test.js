fefabf8a1924be6697d14b9adc921f836e958bf9
Kevin Martensson <kevinmartensson@gmail.com>
2014-01-24 14:15:42 +0100

    Rename imagemin.js to index.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 
 var assert = require('assert');
 var fs = require('fs');
-var imagemin = require('../imagemin');
+var imagemin = require('../');
 var path = require('path');
 var rm = require('rimraf');
 
