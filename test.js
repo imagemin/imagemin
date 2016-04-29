ff8b4e68cca092bacb771f084bafb572c73e1d1c
Kevin Mårtensson <kevinmartensson@gmail.com>
2016-04-29 15:40:01 +0200

    Use promise based API (#162)

---
+++ test.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import imageminJpegtran from 'imagemin-jpegtran';
+import isJpg from 'is-jpg';
+import pify from 'pify';
+import test from 'ava';
+import m from './';
+
+const fsP = pify(fs);
+
+test('optimize a file', async t => {
+	const buf = await fsP.readFile(path.join(__dirname, 'fixture.jpg'));
+	const files = await m(['fixture.jpg'], {use: imageminJpegtran()});
+
+	t.is(files[0].path, null);
+	t.true(files[0].data.length < buf.length);
+	t.true(isJpg(files[0].data));
+});
+
+test('optimize a buffer', async t => {
+	const buf = await fsP.readFile(path.join(__dirname, 'fixture.jpg'));
+	const data = await m.buffer(buf, {use: imageminJpegtran()});
+
+	t.true(data.length < buf.length);
+	t.true(isJpg(data));
+});
+
+test('output error on corrupt images', async t => {
+	t.throws(m(['fixture-corrupt.jpg'], {use: imageminJpegtran()}), /Corrupt JPEG data/);
+});
+
+test('throw on wrong input', async t => {
+	t.throws(m('foo'), /Expected an array/);
+	t.throws(m.buffer('foo'), /Expected a buffer/);
+});
