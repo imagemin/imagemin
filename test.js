b0c4020d1e7867d146da81680be46ca7c3cebc48
Adrian Stainforth <adrian@morsedigital.com>
2016-08-08 21:33:41 +0100

    attempt to fix folder issue

--- test.js
+++ test.js
@@ -74,10 +74,8 @@ test('output at the specified folder location', async t => {
 	let testfolder = 'output2';
 	const tmp = tempfile();
 	const buf = await fsP.readFile(path.join(__dirname, 'fixture.jpg'));
-	await pify(mkdirp)(tmp)
-					.then(data => {
-						mkdirp.sync(path.join(data, 'bar'));
-					});
+	await pify(mkdirp)(tmp);
+	await pify(mkdirp)(path.join(tmp, 'bar'));
 
 	await fsP.writeFile(path.join(tmp, 'bar', 'fixture.jpg'), buf);
 	// await fsP.writeFile(path.join(tmp, 'bar', 'foo', 'fixture.jpg'), buf);
