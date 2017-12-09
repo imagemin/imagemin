302f71af7113f3a0dc0244de47dd3394e8fe0aa5
stfsy <s.pfaffel@gmail.com>
2017-12-09 21:49:28 +0100

    chore: set strict mode

--- test/cwd.spec.js
+++ test/cwd.spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const path = require('path')
 const imageminJpegtran = require('imagemin-jpegtran')
 const tempfile = require('tempfile')
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fs = require('fs-extra')
 const path = require('path')
 const imageminJpegtran = require('imagemin-jpegtran')
